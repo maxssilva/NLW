c1d82ff05490317e857f386f49f43e254d5b8ce8
maxssilva <maxssilva@yahoo.com.br>
2020-10-28 10:16:09 -0300

    inserção dos carrocel de imagens no orphanage.html e também criando o script de busca aleatória de imagens

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -28,5 +28,27 @@ L
 .marker([-27.2119529,-49.6638543], {icon})
 .addTo(map)
 
+/*image galery*/ 
 
+function selectImage(event){
+    const button = event.currentTarget
+    
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button){
+    button.classList.remove("active")
+    }
+
+    //selecionar imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+ 
+    //atualizar o container de image
+    imageContainer.src = image.src
+
+    //adicionar a classe .active
+    button.classList.add('active') 
+}
   
